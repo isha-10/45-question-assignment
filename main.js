c97a1b888adef9f6262f100ef99fbaf659a61afe
isha-10 <165405250+isha-10@users.noreply.github.com>
2024-03-30 13:44:49 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+//  Personal Message: Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric,
+// would you like to learn some Python today?”
+var variable = ("isha");
+console.log("hello" + variable + ",would you like to learn phython today");
