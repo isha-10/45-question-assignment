2373a72f3d3ddd959ddb37bf281cf118cecd7140
isha-10 <165405250+isha-10@users.noreply.github.com>
2024-04-03 11:28:36 -0700

    Add files via upload

--- main.ts
+++ main.ts
@@ -1,1 +1,3 @@
-console.log("william shakespear said, all that glitters are not gold")
+let famousperson = ("william shakespear");
+let message = "once said ,all glitters are not gold ";
+console.log(famousperson, message)
