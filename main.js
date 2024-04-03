9ba56c508018710ab538a316ff0d948414aa3bcd
isha-10 <165405250+isha-10@users.noreply.github.com>
2024-04-03 11:25:54 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,1 @@
-//lower case
-var personName1 = "isha";
-console.log("lowercase", personName1.toLowerCase());
-//upper case
-var personName2 = "isha";
-console.log("uppercase", personName2.toLocaleUpperCase());
-//title case
-var personName3 = "isha";
-console.log("titlecase", personName3.replace(/\b\w/g, function (c) { return c.toUpperCase(); }));
+console.log("william shakespear said, all that glitters are not gold");
