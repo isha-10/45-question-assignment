471532e1e84aa2365044a85f84790df61655186c
isha-10 <165405250+isha-10@users.noreply.github.com>
2024-04-03 11:25:17 -0700

    Add files via upload

--- main.ts
+++ main.ts
@@ -1,14 +1,1 @@
-
-//lower case
- let  personName1 : string = "isha";
-  console.log("lowercase", personName1 . toLowerCase())
-
-
-//upper case
-let personName2 : string= "isha" ;
- console.log("uppercase", personName2 . toLocaleUpperCase());
-
-
-//title case
-let personName3 : string ="isha";
-console.log("titlecase",personName3 . replace (/\b\w/g , c=> c . toUpperCase()));
+console.log("william shakespear said, all that glitters are not gold")
