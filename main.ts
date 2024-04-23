1609902823b7f53ff10b6f06c9ea6f52ea9b38a4
isha khan <165405250+isha-10@users.noreply.github.com>
2024-04-23 03:37:26 -0700

    Add files via upload

--- main.ts
+++ main.ts
@@ -1,2 +1,8 @@
-let favouritenumber = 7
-console.log ("my favourite number" , favouritenumber);
+//addition
+console.log(4+4);
+//substraction
+console.log(10-2);
+//multplication
+console.log(4*2);
+//division
+console.log(16/)
