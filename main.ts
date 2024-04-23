78ae11d9a47caa4373376e991df79bc1cfe8d834
isha khan <165405250+isha-10@users.noreply.github.com>
2024-04-23 03:33:35 -0700

    Add files via upload

--- main.ts
+++ main.ts
@@ -1,3 +1,4 @@
-let famousperson = ("william shakespear");
-let message = "once said ,all glitters are not gold ";
-console.log(famousperson, message)
+let whitespaces = "/t/n isha khan /t/n";
+console.log (whitespaces);
+let withoutwhitespace = whitespaces.trim ();
+console.log (withoutwhitespace );
