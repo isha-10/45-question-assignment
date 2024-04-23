16f4009f36ca7712cb04a56cb2a0e23e69c26edd
isha khan <165405250+isha-10@users.noreply.github.com>
2024-04-23 03:42:21 -0700

    Add files via upload

--- main.ts
+++ main.ts
@@ -1,6 +1,2 @@
-let names : string [] = ["shafay ","shaheer","haniya","hashir"];
-let message : string = "do you like to play football?"
-console.log (names [0] + " " + message)
-console.log( names [1] + " " + message)
-console.log(names [2] +  " " + message)
-console.log( names[3] + " " + message)
+let transpotation : string [] = ["bmw","civics","fortuner"];
+transpotation.map => console.log ()
